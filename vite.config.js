35e79def566f202546d2ff0732c7df41677fb884
asgharkapk <162083661+asgharkapk@users.noreply.github.com>
2025-11-11 07:53:43 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-plugins: [react()],
-base: '/star-resonance-module-optimizer/' // update if repo name differs
-});
+  plugins: [react()],
+  base: '/star-resonance-module-optimizer/', // ⚠️ change to your repo name
+})

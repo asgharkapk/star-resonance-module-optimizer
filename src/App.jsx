164bdda59f8c0fc2b4acbfb37798c2f1d2976eb3
asgharkapk <162083661+asgharkapk@users.noreply.github.com>
2025-11-11 07:45:10 +0330

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TableInput from "./components/TableInput";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white text-black flex justify-center items-start p-8">
+      <TableInput />
+    </div>
+  );
+}
